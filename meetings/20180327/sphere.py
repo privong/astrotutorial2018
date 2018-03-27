ad269aa5d73ccb6c3d2a8161836666eee43a4cb4
George C. Privon <george.privon@ufl.edu>
2018-03-27 18:12:48 -0400

    sphere.py code for a take-home exercise

---
+++ meetings/20180327/sphere.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Have the user input the radius of a sphere. Then compute the volume and
+surface area. Print the volume and surface area to the screen.
+"""
+
+import numpy as np
+
+
+radius = input("What radius is the sphere? ")
+
+radius = float(radius)
+
+# write code here to compute the surface area of the ball and print
+# the number out to the screen
+
+# and the volume
+
